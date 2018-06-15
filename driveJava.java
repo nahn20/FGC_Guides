4e132cccedce6ac9273700ea13b21126f68b9354
nahn20 <nahn20@students.hopkins.edu>
2018-06-15 13:59:01 -0400

    Readded Drive

--- driveJava.java
+++ driveJava.java
@@ -9,6 +9,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class driveJava extends LinearOpMode {
     
     float rotate_angle = 0;
+    double reset_angle = 0;
     
     private DcMotor front_left_wheel = null;
     private DcMotor back_left_wheel = null;
@@ -47,8 +48,8 @@ public class driveJava extends LinearOpMode {
         while(!opModeIsActive()){}
         
         while(opModeIsActive()){
-
             drive();
+            resetAngle();
             //driveSimple();
             telemetry.update();
         }
@@ -93,16 +94,15 @@ public class driveJava extends LinearOpMode {
         }
     }
     public void drive() {
-        double stick_x = gamepad1.left_stick_x / Math.sqrt(2);
-        double stick_y = gamepad1.left_stick_y / Math.sqrt(2);
+        double Protate = gamepad1.right_stick_x/4;
+        double stick_x = gamepad1.left_stick_x * Math.sqrt(Math.pow(1-Math.abs(Protate), 2)/2); //Accounts for Protate when limiting magnitude to be less than 1
+        double stick_y = gamepad1.left_stick_y * Math.sqrt(Math.pow(1-Math.abs(Protate), 2)/2);
         double theta = 0;
         double Px = 0;
         double Py = 0;
-        double Protate = 0;
 
-        
-        double gyroAngle = getHeading() * Math.PI / 180;
-        if (gyroAngle <= 0) {
+        double gyroAngle = getHeading() * Math.PI / 180; //Converts gyroAngle into radians
+        if (gyroAngle <= 0) { 
             gyroAngle = gyroAngle + (Math.PI / 2);
         } else if (0 < gyroAngle && gyroAngle < Math.PI / 2) {
             gyroAngle = gyroAngle + (Math.PI / 2);
@@ -111,9 +111,11 @@ public class driveJava extends LinearOpMode {
         }
         gyroAngle = -1 * gyroAngle;
         
-        if(gamepad1.right_bumper){
+        if(gamepad1.right_bumper){ //Disables gyro, sets to -Math.PI/2 so front is defined correctly. 
             gyroAngle = -Math.PI/2;
         }
+        
+        //Linear directions in case you want to do straight lines.
         if(gamepad1.dpad_right){
             stick_x = 0.5;
         }
@@ -126,32 +128,41 @@ public class driveJava extends LinearOpMode {
         else if(gamepad1.dpad_down){
             stick_y = 0.5;
         }
-        //ROTATION
-        Protate = gamepad1.right_stick_x/4;
+        
         
         //MOVEMENT
-        theta = (Math.atan2(stick_y, stick_x) - gyroAngle) - (Math.PI / 2);
+        theta = Math.atan2(stick_y, stick_x) - gyroAngle - (Math.PI / 2);
         Px = Math.sqrt(Math.pow(stick_x, 2) + Math.pow(stick_y, 2)) * (Math.sin(theta + Math.PI / 4));
         Py = Math.sqrt(Math.pow(stick_x, 2) + Math.pow(stick_y, 2)) * (Math.sin(theta - Math.PI / 4));
+        
+        telemetry.addData("Stick_X", stick_x);
+        telemetry.addData("Stick_Y", stick_y);
         telemetry.addData("Magnitude",  Math.sqrt(Math.pow(stick_x, 2) + Math.pow(stick_y, 2)));
         telemetry.addData("Front Left", Py - Protate);
         telemetry.addData("Back Left", Px - Protate);
         telemetry.addData("Back Right", Py + Protate);
         telemetry.addData("Front Right", Px + Protate);
+        
         front_left_wheel.setPower(Py - Protate);
         back_left_wheel.setPower(Px - Protate);
         back_right_wheel.setPower(Py + Protate);
         front_right_wheel.setPower(Px + Protate);
     }
+    public void resetAngle(){
+        if(gamepad1.a){
+            reset_angle = getHeading() + reset_angle;
+        }
+    }
     public double getHeading(){
         Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-        float heading = angles.firstAngle;
+        double heading = angles.firstAngle;
         if(heading < -180) {
             heading = heading + 360;
         }
         else if(heading > 180){
             heading = heading - 360;
         }
+        heading = heading - reset_angle;
         return heading;
     }
 }
